f4b272864868bd048745dcf6c0c0384cbd454426
chelobureck <adylbekoveldiyar2@gmail.com>
2025-07-04 12:38:53 +0600

    fix bag in config/settings.py/Settings/DATEBASE_URL

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,7 @@ class Settings(BaseSettings):
     
     @property
     def DATABASE_URL(self) -> str:
-        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}?ssl=require"
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}?sslmode=require"
     
     class Config:
         env_file = ".env"
